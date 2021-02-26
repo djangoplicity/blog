1a15e241b82f55048474a51c8584a4c86ba4c95a
Edison Arango <edisonarab@gmail.com>
2021-02-25 22:45:10 -0500

    Feature/blog video and did you know template tags

--- djangoplicity/blog/templatetags/blog_utils.py
+++ djangoplicity/blog/templatetags/blog_utils.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.html import format_html
 from djangoplicity.blog.models import Category
 
 register = template.Library()
@@ -9,3 +10,23 @@ def list_blog_categories():
     return {
         "categories": Category.objects.all()
     }
+
+
+@register.simple_tag
+def blog_video(src, **kwargs):
+    fullwidth = kwargs.get('fullwidth', 'False') == 'True'
+    html_class = 'fullwidth' if fullwidth else ''
+
+    if not src:
+        return ''
+    return format_html(
+        '<video src="{}" class="{}" autoplay="autoplay" loop="loop" muted="muted" width="100%"></video>',
+        src, html_class
+    )
+
+
+@register.simple_tag
+def dyk(text):
+    if not text:
+        return ''
+    return format_html('<div class="dyk"><span class="dyk__title">#DYK</span><p>{}</p></div>', text)
